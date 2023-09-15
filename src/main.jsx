3cf9a8313a8f44d47eee42bace5ee79680831f98
Inna Nomerovkina <inkosof@gmail.com>
2023-09-15 09:50:24 +0300

    Revert "fix"
    This reverts commit 605c49bcaf03e8181b1589ffd99ab824d3682736.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './styles/reset.scss';
 import './styles/styles.scss';
+// import './index.css'
 
-
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
